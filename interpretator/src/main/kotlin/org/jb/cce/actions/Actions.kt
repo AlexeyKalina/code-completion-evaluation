c4a9326ae1b93b909bbaea2736d065c8bca4f6d0
Alexey Kalina <alexey.kalina96@gmail.com>
2019-05-02 11:30:55 +0300

    call completion action as data class

--- interpretator/src/main/kotlin/org/jb/cce/actions/Actions.kt
+++ interpretator/src/main/kotlin/org/jb/cce/actions/Actions.kt
@@ -9,7 +9,7 @@ sealed class Action(val type: ActionType) {
 }
 
 data class MoveCaret(val offset: Int) : Action(ActionType.MOVE_CARET)
-class CallCompletion(val text: String, val expectedText: String,
+data class CallCompletion(val text: String, val expectedText: String,
                      val completionType: CompletionType, val tokenType: TokenType) : Action(ActionType.CALL_COMPLETION)
 class CancelSession : Action(ActionType.CANCEL_SESSION)
 data class PrintText(val text: String) : Action(ActionType.PRINT_TEXT)
