2fef6450e61aac4671a32c5769690d8d2306d152
Alexey Kalina <alexey.kalina@jetbrains.com>
2019-08-12 11:00:33 +0300

    remove bad interpreting metrics

--- metrics-evaluator/src/main/kotlin/org/jb/cce/metrics/MetricsEvaluator.kt
+++ metrics-evaluator/src/main/kotlin/org/jb/cce/metrics/MetricsEvaluator.kt
@@ -14,10 +14,9 @@ class MetricsEvaluator private constructor() {
     private val metrics = mutableListOf<Metric>()
 
     fun registerDefaultMetrics() {
-        registerMetric(PrecisionMetric())
+        registerMetric(PrecisionOneMetric())
+        registerMetric(PrecisionFiveMetric())
         registerMetric(RecallMetric())
-        registerMetric(FMeasureMetric())
-        registerMetric(MeanReciprocalRankMetric())
         registerMetric(ESavedMetric())
         registerMetric(MeanLatencyMetric())
         registerMetric(MaxLatencyMetric())
--- metrics-evaluator/src/main/kotlin/org/jb/cce/metrics/PrecisionMetric.kt
+++ metrics-evaluator/src/main/kotlin/org/jb/cce/metrics/PrecisionMetric.kt
@@ -4,14 +4,13 @@ import org.jb.cce.Session
 import org.jb.cce.metrics.util.Sample
 import java.util.stream.Collectors
 
-class PrecisionMetric : Metric {
+abstract class PrecisionMetric : Metric {
     private val sample = Sample()
 
     override val value: Double
         get() = sample.mean()
 
     override fun evaluate(sessions: List<Session>): Double {
-        // top3
         val listOfCompletions = sessions.stream()
                 .flatMap { session -> session.lookups.map { lookup -> Pair(lookup.suggestions, session.expectedText) }.stream() }
                 .collect(Collectors.toList())
@@ -19,7 +18,7 @@ class PrecisionMetric : Metric {
         val fileSample = Sample()
         for (completion in listOfCompletions) {
             val indexOfNecessaryCompletion = completion.first.map { it.text }.indexOf(completion.second)
-            if (indexOfNecessaryCompletion in 0..3) {
+            if (indexOfNecessaryCompletion in 0 until level()) {
                 fileSample.add(1.0)
                 sample.add(1.0)
             } else if (completion.first.isNotEmpty()) {
@@ -31,5 +30,15 @@ class PrecisionMetric : Metric {
         return fileSample.mean()
     }
 
-    override val name: String = "Precision"
+    abstract fun level(): Int
+}
+
+class PrecisionOneMetric : PrecisionMetric() {
+    override fun level() = 1
+    override val name: String = "Precision@1"
+}
+
+class PrecisionFiveMetric : PrecisionMetric() {
+    override fun level() = 5
+    override val name: String = "Precision@5"
 }
