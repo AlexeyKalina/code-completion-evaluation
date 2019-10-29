5bd12fe02890b16c8c4d267ac4b117dade7795ee
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2019-10-29 07:09:43 +0300

    Make length of a progress bar constant

--- completion-plugin/src/org/jb/cce/util/CommandLineProgress.kt
+++ completion-plugin/src/org/jb/cce/util/CommandLineProgress.kt
@@ -1,9 +1,5 @@
 package org.jb.cce.util
 
-import java.lang.String.join
-import java.util.Collections.nCopies
-import kotlin.math.log10
-
 class CommandLineProgress(private val title: String) : Progress {
     companion object {
         private const val progressLength = 20
@@ -20,14 +16,15 @@ class CommandLineProgress(private val title: String) : Progress {
         currentFile = fileName
 
         progress.clear()
+        val doneParts = (fraction * (progressLength - 1)).toInt()
         progress
                 .append('\r')
                 .append("$title:")
-                .append(join("", nCopies(if (percent == 0) 2 else 2 - log10(percent.toDouble()).toInt(), " ")))
-                .append(String.format(" %d%% [", percent))
-                .append(join("", nCopies(percent * progressLength / 100, "=")))
+                .append("${percent.toString().padStart(3)}%")
+                .append(" [")
+                .append("=".repeat(doneParts))
                 .append('>')
-                .append(join("", nCopies((100 - percent) * progressLength / 100, " ")))
+                .append(" ".repeat(progressLength - doneParts - 1))
                 .append("] $text")
         println(progress)
     }
