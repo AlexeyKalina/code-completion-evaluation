b58bb186b17300ee04d6a470f6e2297b01d56c2f
Alexey Kalina <alexey.kalina@jetbrains.com>
2019-11-11 21:55:05 +0300

    don't suggest to choose report if it's single

--- completion-plugin/src/org/jb/cce/dialog/OpenBrowserDialog.kt
+++ completion-plugin/src/org/jb/cce/dialog/OpenBrowserDialog.kt
@@ -5,21 +5,30 @@ import com.intellij.ui.layout.panel
 import javax.swing.JComponent
 
 class OpenBrowserDialog(private val reportNames: List<String>) : DialogWrapper(true) {
+    val reportNamesForOpening = mutableSetOf<String>()
+
     init {
         init()
         title = "Quality evaluation completed"
     }
 
-    val reportNamesForOpening = mutableSetOf<String>()
-
-    override fun createCenterPanel(): JComponent? = panel {
-        row { label("Select reports for opening in the browser:") }
-        for (reportName in reportNames) {
-            row {
-                checkBox(reportName).apply {
-                    addItemListener {
-                        if (isSelected) reportNamesForOpening.add(text)
-                        else reportNamesForOpening.remove(text)
+    override fun createCenterPanel(): JComponent? {
+        if (reportNames.size == 1) {
+            reportNamesForOpening.add(reportNames.first())
+            return panel {
+                row { label("Do you want to open a folder with reports?") }
+            }
+        } else {
+            return panel {
+                row { label("Select reports for opening in the browser:") }
+                for (reportName in reportNames) {
+                    row {
+                        checkBox(reportName).apply {
+                            addItemListener {
+                                if (isSelected) reportNamesForOpening.add(text)
+                                else reportNamesForOpening.remove(text)
+                            }
+                        }
                     }
                 }
             }
