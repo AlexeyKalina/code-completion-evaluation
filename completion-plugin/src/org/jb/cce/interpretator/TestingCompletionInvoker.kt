381abc58512f7890a8fe6cec096a806756d0bb4d
Alexey Kalina <alexey.kalina@jetbrains.com>
2019-11-15 14:07:34 +0300

    use default kotlin delegator

--- completion-plugin/src/org/jb/cce/interpretator/TestingCompletionInvoker.kt
+++ completion-plugin/src/org/jb/cce/interpretator/TestingCompletionInvoker.kt
@@ -3,25 +3,9 @@ package org.jb.cce.interpretator
 import org.jb.cce.CompletionInvoker
 import org.jb.cce.Lookup
 
-class TestingCompletionInvoker(private val invoker: CompletionInvoker) : CompletionInvoker {
+class TestingCompletionInvoker(private val invoker: CompletionInvoker) : CompletionInvoker by invoker {
     override fun callCompletion(expectedText: String, prefix: String): Lookup {
         val lookup = invoker.callCompletion(expectedText, prefix)
         return Lookup(lookup.text, lookup.suggestions.take(1), 0)
     }
-
-    override fun moveCaret(offset: Int) = invoker.moveCaret(offset)
-
-    override fun finishCompletion(expectedText: String, prefix: String): Boolean = invoker.finishCompletion(expectedText, prefix)
-
-    override fun printText(text: String) = invoker.printText(text)
-
-    override fun deleteRange(begin: Int, end: Int) = invoker.deleteRange(begin, end)
-
-    override fun openFile(file: String): String = invoker.openFile(file)
-
-    override fun closeFile(file: String) = invoker.closeFile(file)
-
-    override fun isOpen(file: String): Boolean = invoker.isOpen(file)
-
-    override fun getText(): String = invoker.getText()
 }
