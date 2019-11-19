ee3313cef361cc3a8e59542eb500f84c79b9f521
Alexey Kalina <alexey.kalina@jetbrains.com>
2019-11-19 11:42:14 +0300

    save logs without losses

--- completion-plugin/src/org/jb/cce/evaluation/ActionsInterpretationHandler.kt
+++ completion-plugin/src/org/jb/cce/evaluation/ActionsInterpretationHandler.kt
@@ -1,9 +1,14 @@
 package org.jb.cce.evaluation
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.registry.Registry
+import com.intellij.serviceContainer.PlatformComponentManagerImpl
+import com.intellij.stats.sender.StatisticSender
+import com.intellij.stats.storage.FilePathProvider
+import com.intellij.stats.storage.UniqueFilesProvider
 import org.jb.cce.Config
 import org.jb.cce.EvaluationWorkspace
 import org.jb.cce.InterpretFilter
@@ -18,6 +23,7 @@ import org.jb.cce.interpretator.InterpretationHandlerImpl
 import org.jb.cce.util.FilesHelper
 import org.jb.cce.util.Progress
 import org.jb.cce.util.text
+import java.io.File
 import java.nio.file.Paths
 import java.util.*
 import kotlin.system.measureTimeMillis
@@ -45,8 +51,7 @@ class ActionsInterpretationHandler(
         val mlCompletionFlag = isMLCompletionEnabled()
         LOG.info("Start interpreting actions")
         setMLCompletion(config.completionType == CompletionType.ML)
-        if (config.saveLogs)
-            workspace2.logsStorage.watch(statsCollectorLogsDirectory(), language, config.trainTestSplit)
+        if (config.saveLogs) replaceStatCollectorServices()
         val files = workspace1.actionsStorage.getActionFiles()
         for (file in files) {
             val fileActions = workspace1.actionsStorage.getActions(file)
@@ -60,7 +65,7 @@ class ActionsInterpretationHandler(
             }
             if (handler.isCancelled()) break
         }
-        if (config.saveLogs) workspace2.logsStorage.stopWatching()
+        if (config.saveLogs) workspace2.logsStorage.save(statsCollectorLogsDirectory(), language, config.trainTestSplit)
         workspace2.sessionsStorage.saveEvaluationInfo()
         LOG.info("Interpreting actions completed")
         setMLCompletion(mlCompletionFlag)
@@ -82,5 +87,18 @@ class ActionsInterpretationHandler(
             Registry.get(PropertKey@ "completion.stats.enable.ml.ranking").setValue(value)
         } catch (e: MissingResourceException) {
         }
+    }
+
+    private fun replaceStatCollectorServices() {
+        File(statsCollectorLogsDirectory()).deleteRecursively()
+        val serviceManager = ApplicationManager.getApplication() as PlatformComponentManagerImpl
+        val filesProvider = object : UniqueFilesProvider("chunk", PathManager.getSystemPath(), "completion-stats-data") {
+            override fun cleanupOldFiles() = Unit
+        }
+        val statisticSender = object : StatisticSender {
+            override fun sendStatsData(url: String) = Unit
+        }
+        serviceManager.replaceServiceInstance(FilePathProvider::class.java, filesProvider, project)
+        serviceManager.replaceServiceInstance(StatisticSender::class.java, statisticSender, project)
     }
 }
